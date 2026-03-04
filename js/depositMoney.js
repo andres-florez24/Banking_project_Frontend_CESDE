cb0ae7a12fe76eb49d95600bdc8e8d9e6771cc2c
sebastian sepulveda <sebastiian.sepulveda1@gmail.com>
2026-03-03 22:12:57 -0500

    agrego la parte de consignar

---
+++ js/depositMoney.js
@@ -0,0 +1,19 @@
+let saldo = 10000;
+
+function consignar() {
+    let monto = Number(prompt("Ingrese el monto a consignar:"));
+
+    if (monto > 0) {
+
+        saldo += monto;
+
+        console.log("Consignación exitosa.");
+        console.log("Monto consignado: $" + monto);
+        console.log("Nuevo saldo: $" + saldo);
+
+    } else {
+        console.log(" Monto inválido");
+    }
+}
+
+consignar()
